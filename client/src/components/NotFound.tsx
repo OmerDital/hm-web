8a5a7c52150f4b34f59e6491e33add4c67dbc336
benben12341 <benpro214@gmail.com>
2021-12-02 17:07:22 +0200

    feat(404-page):add 404 page and redirect button to home page

--- client/src/components/NotFound.tsx
+++ client/src/components/NotFound.tsx
@@ -1,3 +1,29 @@
-const NotFound = () => (<h1>404</h1>);
+import { Button, Grid, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Grid
+      container
+      direction='column'
+      sx={{
+        alignItems: 'center',
+        justifyContent: 'space-around'
+      }}
+    >
+      <Typography fontSize='216px' color='rgb(0, 94, 93)'>
+        404
+      </Typography>
+      <Typography variant='h5'>
+        {' אופס! הדף שביקשת אינו נמצא, אנא חזור לדף הבית ונסה שנית  '}
+      </Typography>
+      <Button variant='contained' color='success' onClick={() => navigate('/')}>
+        לדף הבית
+      </Button>
+    </Grid>
+  );
+};
 
 export default NotFound;
