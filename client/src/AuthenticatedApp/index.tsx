8ef8ce135121a747089ae44c78058da5c9ce0772
Noga lavan <noga0802@gmail.com>
2022-01-06 13:45:14 +0200

    feat(manage-entities) add manage entities basic view page

--- client/src/AuthenticatedApp/index.tsx
+++ client/src/AuthenticatedApp/index.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import DistributeSong from './DistributeSong';
 import ManageEntities from './ManageEntities';
+import ManageAlbums from './ManageEntities/ManageAlbums';
 import Calculators from './Calculators';
 import Shell from './Shell';
 import Users from './Users';
@@ -12,6 +13,7 @@ const ShellRoutes = () => (
     <Route path='/' element={<Shell />}>
       <Route index element={<ManageEntities />} />
       <Route path='manage-entities' element={<ManageEntities />} />
+      <Route path='manage-entities/manage-albums' element={<ManageAlbums />} />
       <Route path='distribute-song' element={<DistributeSong />} />
       <Route path='calculators' element={<Calculators />} />
       <Route path='calculators/love-calculator' element={<LoveCalculator />} />
