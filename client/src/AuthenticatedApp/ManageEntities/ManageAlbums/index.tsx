e5ae800bd290a6838d90320c083d0aabc4f762e3
Noga lavan <noga0802@gmail.com>
2022-01-09 17:26:36 +0200

    feat(manage-entities): add manage entities basic view page

---
+++ client/src/AuthenticatedApp/ManageEntities/ManageAlbums/index.tsx
@@ -0,0 +1,5 @@
+const ManageAlbums = () => (
+  <h1>ניהול אלבומים</h1>
+);
+
+export default ManageAlbums;
--- client/src/AuthenticatedApp/ManageEntities/index.tsx
+++ client/src/AuthenticatedApp/ManageEntities/index.tsx
@@ -1,5 +1,27 @@
+import { Grid } from '@mui/material';
+import CardLink from '../CardLink';
+
 const ManageEntities = () => (
-  <h1>ניהול ישויות</h1>
+  <Grid container>
+    <Grid item xs={12} sm={4}>
+      <CardLink to='/manage-entities/manage-albums'>אלבומים</CardLink>
+    </Grid>
+    <Grid item xs={12} sm={4}>
+      <CardLink to='/manage-entities/manage-albums'>נקודות עבודה</CardLink>
+    </Grid>
+    <Grid item xs={12} sm={4}>
+      <CardLink to='/manage-entities/manage-albums'>כלי נגינה</CardLink>
+    </Grid>
+    <Grid item xs={12} sm={4}>
+      <CardLink to='/manage-entities/manage-albums'>תו נלווה</CardLink>
+    </Grid>
+    <Grid item xs={12} sm={4}>
+      <CardLink to='/manage-entities/manage-albums'>תו מוביל</CardLink>
+    </Grid>
+    <Grid item xs={12} sm={4}>
+      <CardLink to='/manage-entities/manage-albums'>משתנים</CardLink>
+    </Grid>
+  </Grid>
 );
 
 export default ManageEntities;
